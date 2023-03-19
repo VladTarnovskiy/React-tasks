8218c161cf9d57758bf2dc46b3d2396aee9a2fd5
Vlad Tarnovski <vladtarnovski989@gmail.com>
2023-03-19 16:15:38 +0300

    feat: error page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
-import { HashRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes, NavLink } from 'react-router-dom';
 import Home from './pages/Home';
-import NotFound from './pages/NotFound';
+import NotFound from './pages/NotFound/NotFound';
 import Header from './components/Header';
+import AboutUs from './pages/AboutUs';
 
 export function App() {
   return (
     <div>
+      <div className="navigation">
+        <NavLink className="nav__item" to="/">
+          Go Home
+        </NavLink>
+        <NavLink className="nav__item" to="/about-us">
+          About Us
+        </NavLink>
+      </div>
       <Header title="About us" />
       <Routes>
+        <Route path="/about-us" element={<AboutUs />} />
         <Route path="/" element={<Home />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
---
+++ src/pages/AboutUs.tsx
@@ -0,0 +1,9 @@
+function AboutUs() {
+  return (
+    <div>
+      <div className="about__us">We are technical company.</div>
+    </div>
+  );
+}
+
+export default AboutUs;
