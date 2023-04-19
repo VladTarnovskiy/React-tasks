4a456594a7ad6ec9f186eaab2077bc78a704f58d
Vlad Tarnovski <vladtarnovski989@gmail.com>
2023-04-19 21:39:27 +0300

    refactor: remove comment code

--- src/app/store.ts
+++ src/app/store.ts
@@ -13,29 +13,3 @@ export const store = configureStore({
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
-
-// import { combineReducers, configureStore, PreloadedState } from '@reduxjs/toolkit';
-// import searchBarReducer from '../components/searchBar/searchBarSlice';
-// import homeCardsReducer from '../components/homeCard/homeCardsSlice';
-// import formReducer from '../components/Form/formSlice';
-
-// const rootReducer = combineReducers({
-//   searchBarValue: searchBarReducer,
-//   homeCards: homeCardsReducer,
-//   formData: formReducer,
-// });
-
-// // export default configureStore({
-// //   reducer: rootReducer,
-// // });
-
-// export default function setupStore(preloadedState?: PreloadedState<RootState>) {
-//   return configureStore({
-//     reducer: rootReducer,
-//     preloadedState,
-//   });
-// }
-
-// export type RootState = ReturnType<typeof rootReducer>;
-// export type AppStore = ReturnType<typeof setupStore>;
-// export type AppDispatch = AppStore['dispatch'];
