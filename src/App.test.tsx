3815e3ec01e714108c2d58f1c1c12931b34e2eb2
Vlad Tarnovski <vladtarnovski989@gmail.com>
2023-05-02 01:12:00 +0300

    refactor: test files

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,15 +1,18 @@
 import { describe } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { store } from './app/store';
 
 describe('App', () => {
   test('App text', () => {
     render(
-      <Provider store={store}>
-        <App />
-      </Provider>
+      <BrowserRouter>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </BrowserRouter>
     );
     const homeElement = screen.getByText('Go Home');
     expect(homeElement).toBeInTheDocument();
